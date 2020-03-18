226d49479eb21677a279b81ecc85f80a4717e7c3
Wonki Kim <wonki_.kim@samsung.com>
2020-03-18 14:01:23 +0900

    bootstrap: modify to check a unchecked return value.
    this path modifies to check an unchcked return value of an api.
    
    Change-Id: I12b22e730c950b65312b68e20257a278497a22d8

--- bootstrap/server/src/Commands/GetAppInfoCommand.cc
+++ bootstrap/server/src/Commands/GetAppInfoCommand.cc
@@ -22,34 +22,38 @@ GetAppInfoCommand::GetAppInfoCommand(const ::aurum::ReqGetAppInfo* request,
     app_state_e    appState;
 
     char* label = nullptr;
-    bool  terminated = false;
     int   ret = -1;
 
     mResponse->set_status(::aurum::RspStatus::OK);
+    mResponse->set_isinstalled(false);
+    mResponse->set_isrunning(false);
+    mResponse->set_isfocused(false);
 
-    package_manager_get_package_info(packageName.c_str(), &package_info);
-    package_info_get_label(package_info, &label);
-    if (label) {
-        free(label);
-        mResponse->set_isinstalled(true);
+
+    ret = package_manager_get_package_info(packageName.c_str(), &package_info);
+    if (!ret) {
+        package_info_get_label(package_info, &label);
+        if (label) {
+            free(label);
+            mResponse->set_isinstalled(true);
+        } else {
+            return grpc::Status::OK;
+        }
     } else {
-        mResponse->set_isinstalled(false);
         return grpc::Status::OK;
     }
 
     ret = app_manager_get_app_context(packageName.c_str(), &app_context);
-    if (ret) {
-        mResponse->set_isrunning(false);
+    if (!ret) {
+        mResponse->set_isrunning(!(appState & APP_STATE_TERMINATED));
+    } else {
         return grpc::Status::OK;
     }
 
     ret = app_context_get_app_state(app_context, &appState);
-    if (ret) {
-        mResponse->set_status(::aurum::RspStatus::ERROR);
-        return grpc::Status::OK;
+    if (!ret) {
+        mResponse->set_isfocused(appState & APP_STATE_FOREGROUND);
     }
-    mResponse->set_isrunning(!(appState & APP_STATE_TERMINATED));
-    mResponse->set_isfocused(appState & APP_STATE_FOREGROUND);
 #endif
     return grpc::Status::OK;
 }
