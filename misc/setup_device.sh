f926d6f4d49d31da03a2723d630824b36670eec8
Wonki Kim <wonki_.kim@samsung.com>
2020-03-23 18:14:57 +0900

    misc: update a helper script
    Change-Id: Ibc9e4fd493fad0de439277d7716abb537b619369

--- misc/setup_device.sh
+++ misc/setup_device.sh
@@ -2,5 +2,5 @@
 
 sdb forward --remove-all
 sdb forward tcp:50051 tcp:50051
-sdb shell "su - owner -c 'systemctl --user start aurum-bootstrap'"
+sdb shell 'su - owner -c "systemctl --user start aurum-bootstrap"'
 sdb shell "ps -ef | grep aurum-bootstrap"
