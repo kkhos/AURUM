ded2273ad4dda725b71962b9417569f5a9ab6f94
Wonki Kim <wonki_.kim@samsung.com>
2020-03-25 10:22:10 +0900

    libaurum: add a missing header
    string.h is needed to use strdup
    
    Change-Id: Icf6e14ea3a5425054c4c817ba72886ffb058c167

--- libaurum/src/AccessibleUtils.cc
+++ libaurum/src/AccessibleUtils.cc
@@ -1,5 +1,6 @@
 #include "AccessibleUtils.h"
 #include "loguru.hpp"
+#include <string.h>
 
 char* state_to_char(AtspiStateType state)
 {
@@ -99,7 +100,7 @@ char* state_to_char(AtspiStateType state)
    }
 }
 
-static void debug( AtspiStateSet *stateSet)
+static void _print_stateset_debug( AtspiStateSet *stateSet)
 {
    if (!stateSet) return;
    GArray *states = atspi_state_set_get_states(stateSet);
