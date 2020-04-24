d53f19c80ec4fb68840b1476afdcd2eb333c3e3e
Wonki Kim <wonki_.kim@samsung.com>
2020-04-24 18:58:28 +0900

    example: rename a script to use it as internal test
    there is a script that calls commands which are for internal use only.
    this patch rename the script as according purpose
    
    Change-Id: Iacea3beae4fdab364ec7be35ab640a3eb076cb02

--- protocol/examples/python/testInternal.py
+++ protocol/examples/python/testInternal.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import aurum_pb2
+from aurum_pb2 import *
 import aurum_pb2_grpc
 import logging
 import grpc
@@ -7,7 +7,10 @@ import grpc
 def run():
     with grpc.insecure_channel('127.0.0.1:50051') as channel:
         stub = aurum_pb2_grpc.BootstrapStub(channel)
-        stub.killServer(aurum_pb2.ReqEmpty())
+
+        print(stub.sync(ReqEmpty()))
+        print(stub.getDeviceTime(ReqGetDeviceTime(type='WALLCLOCK')))
+#        stub.killServer(aurum_pb2.ReqEmpty())
 
 if __name__ == '__main__':
     logging.basicConfig()
