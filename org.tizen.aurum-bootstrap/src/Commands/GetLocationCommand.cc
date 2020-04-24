ffaf1d3e437d8e279bd8adc9e553fbd937d6f56f
Wonki Kim <wonki_.kim@samsung.com>
2020-04-24 22:05:21 +0900

    example: enhance a test script and mismatching protocol
    this patch add a bit more tests
    
    Change-Id: Ib3c82e2e3789302583e60db805b620b9e7116efa

--- org.tizen.aurum-bootstrap/src/Commands/GetLocationCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/GetLocationCommand.cc
@@ -11,5 +11,10 @@ GetLocationCommand::GetLocationCommand(const ::aurum::ReqGetLocation* request,
 {
     LOG_SCOPE_F(INFO, "CliGetLocation --------------- ");
     // ObjectMapper *mObjMap = ObjectMapper::getInstance();
+
+    mResponse->set_alt(-1);
+    mResponse->set_lat(-1);
+    mResponse->set_status(::aurum::RspStatus::ERROR);
+
     return grpc::Status::OK;
 }
