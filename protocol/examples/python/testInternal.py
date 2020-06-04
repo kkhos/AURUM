7c7e6b0ff1bbddd29194b6ab2290be7627e6cce0
Wonki Kim <wonki_.kim@samsung.com>
2020-06-04 20:59:27 +0900

    example/python: modify test scripts for reflecting recent changes
    Change-Id: Ic240aafe580a152823489eb84cdc0cc3705555f1

--- protocol/examples/python/testInternal.py
+++ protocol/examples/python/testInternal.py
@@ -3,15 +3,22 @@ from aurum_pb2 import *
 import aurum_pb2_grpc
 import logging
 import grpc
+import time
 
 def run():
     with grpc.insecure_channel('127.0.0.1:50051') as channel:
         stub = aurum_pb2_grpc.BootstrapStub(channel)
 
-        print(stub.getLocation(ReqGetLocation()).status)
-        print(stub.sync(ReqEmpty()))
-        print(stub.getDeviceTime(ReqGetDeviceTime(type='WALLCLOCK')))
-        # stub.killServer(aurum_pb2.ReqEmpty())
+#        print(stub.getLocation(ReqGetLocation()).status)
+#        print(stub.sync(ReqEmpty()))
+#        print(stub.getDeviceTime(ReqGetDeviceTime(type='WALLCLOCK')))
+#        print(stub.sendKey(ReqKey(type='WHEELUP')))
+#        time.sleep(0.1)
+#        print(stub.sendKey(ReqKey(type='WHEELDOWN')))
+#        time.sleep(0.1)
+#        print(stub.sendKey(ReqKey(type='HOME')))
+#        print(stub.sendKey(ReqKey(type='POWER')))
+#        stub.killServer(aurum_pb2.ReqEmpty())
 
 if __name__ == '__main__':
     logging.basicConfig()
