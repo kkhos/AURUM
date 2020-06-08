0d3ace7c6c725204f3c7299b3a2995ccbc5c9ca6
Wonki Kim <wonki_.kim@samsung.com>
2020-06-08 21:08:53 +0900

    libaurum: refactor a flick command and implementation
    this patch refactor a flick command and its implementation
    it's designed like below.
    
    ^drag start                                      ^drag stop
    |<----------------- 100ms minimum -------------->|
    ***-------++--------++++------++--------+++-------###
    |<------>|<------->|<------->|<------->|<------->|
      ^25ms minimum for each
    
     *:touch down
     +:touch move
     #:touch up
    
    Change-Id: Ibeebf61268177ae9ed5dfa368ee21a140381796b

--- org.tizen.aurum-bootstrap/src/Commands/FlickCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/FlickCommand.cc
@@ -18,7 +18,7 @@ FlickCommand::FlickCommand(const ::aurum::ReqFlick *request,
     int                   durationMs = mRequest->durationms();
 
     UiDevice *device = UiDevice::getInstance(DeviceType::DEFAULT);
-    device->drag(startPoint.x(), startPoint.y(), endPoint.x(), endPoint.y(), durationMs/15, durationMs);
+    device->drag(startPoint.x(), startPoint.y(), endPoint.x(), endPoint.y(), durationMs/MINIMUM_TOUCHEVENT_INTV_MS, durationMs);
     mResponse->set_status(::aurum::RspStatus::OK);
 
     return grpc::Status::OK;
