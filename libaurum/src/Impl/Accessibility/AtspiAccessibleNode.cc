d4ed54f8f8ab586b1fcedd433bee5fe4baf44451
Hosang Kim <hosang12.kim@samsung.com>
2021-04-09 15:58:16 +0900

    libaurum: apply pkg name for accessible node.
    Change-Id: I3338a6a00421f14911e2f33f0eae63b2663c6b0c

--- libaurum/src/Impl/Accessibility/AtspiAccessibleNode.cc
+++ libaurum/src/Impl/Accessibility/AtspiAccessibleNode.cc
@@ -174,7 +174,9 @@ void AtspiAccessibleNode::refresh()
 
         gchar *name = AtspiWrapper::Atspi_accessible_get_name(mNode, NULL);
         mText = name;
-        mPkg = name;
+        AtspiAccessible *app = AtspiWrapper::Atspi_accessible_get_application(mNode, NULL);
+        gchar *pkg = AtspiWrapper::Atspi_accessible_get_name(app, NULL);
+        mPkg = pkg;
         g_free(name);
 
         GHashTable *attributes = AtspiWrapper::Atspi_accessible_get_attributes(mNode, NULL);
@@ -388,3 +390,4 @@ void AtspiAccessibleNode::setFeatureProperty(AtspiStateType type)
         break;
     }
 }
+
