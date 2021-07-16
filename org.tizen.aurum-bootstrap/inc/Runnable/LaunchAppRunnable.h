4054695f660226195d2a2b05aa6abab129e97d15
Woochanlee <wc0917.lee@samsung.com>
2021-07-16 17:54:52 +0900

    bootstrap: Add feature to send additional data to app_control
    Add app_control_add_extra_data() wrapper.
    
    Change-Id: I934968c149cd9d9f0745ffb8ae83f7bd5aba17f8

--- org.tizen.aurum-bootstrap/inc/Runnable/LaunchAppRunnable.h
+++ org.tizen.aurum-bootstrap/inc/Runnable/LaunchAppRunnable.h
@@ -7,9 +7,11 @@
 class LaunchAppRunnable : public Runnable {
 protected:
     std::string mPkg;
+    std::string mKey;
+    std::string mValue;
 
 public:
-    LaunchAppRunnable(std::string pkg);
+    LaunchAppRunnable(std::string pkg, std::string key, std::string value);
     std::string getPkgName();
     void run() const override;
 };
--- org.tizen.aurum-bootstrap/src/Runnable/LaunchAppRunnable.cc
+++ org.tizen.aurum-bootstrap/src/Runnable/LaunchAppRunnable.cc
@@ -7,8 +7,8 @@
 #include <app_control.h>
 #endif
 
-LaunchAppRunnable::LaunchAppRunnable(std::string pkg)
-    : mPkg{pkg}
+LaunchAppRunnable::LaunchAppRunnable(std::string pkg, std::string key, std::string value)
+    : mPkg{pkg}, mKey{key}, mValue{value}
 {
 }
 
@@ -23,20 +23,29 @@ void LaunchAppRunnable::run() const
 
     ret = app_control_create(&appControl);
     if (ret) {
-        LOGE("Launch Failed(1/3) Err Code : %ull", ret);
+        LOGE("Launch Failed(1/4) Err Code : %ull", ret);
         return;
+    }
+
+    if (!mKey.empty() && !mValue.empty()) {
+        ret = app_control_add_extra_data(appControl, mKey.c_str(), mValue.c_str());
+        if (ret) {
+            LOGE("Launch Failed(2/4) Err Code : %ull", ret);
+            app_control_destroy(appControl);
+            return;
+        }
     }
 
     ret = app_control_set_app_id(appControl, packageName.c_str());
     if (ret) {
-        LOGE("Launch Failed(2/3) Err Code : %ull", ret);
+        LOGE("Launch Failed(3/4) Err Code : %ull", ret);
         app_control_destroy(appControl);
         return;
     }
 
     ret = app_control_send_launch_request(appControl, NULL, NULL);
     if (ret) {
-        LOGE("Launch Failed(3/3) Err Code : %ull", ret);
+        LOGE("Launch Failed(4/4) Err Code : %ull", ret);
         app_control_destroy(appControl);
         return;
     }
