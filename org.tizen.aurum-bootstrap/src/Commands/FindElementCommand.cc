d049c336b8bc2e766f4fb20b1b36b58fe24e91e0
Woochanlee <wc0917.lee@samsung.com>
2021-12-20 21:00:04 +0900

    aurum-service: Add textPartialMatch interface to search part of text in full text
    There was a constraint that the entire text must match.
    It is a function that improves usability through interface expansion.
    
    Change-Id: I8b4b996aa4d3a3badbe118c2fee44892a74a830a

--- org.tizen.aurum-bootstrap/src/Commands/FindElementCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/FindElementCommand.cc
@@ -45,24 +45,25 @@ std::vector<std::shared_ptr<UiSelector>> FindElementCommand::getSelectors(void)
 {
     auto sel = std::make_shared<UiSelector>();
 
-    if(mRequest->_elementid_case())       sel->id(mRequest->elementid());
-    if(mRequest->_automationid_case())    sel->automationid(mRequest->automationid());
-    if(mRequest->_textfield_case())       sel->text(mRequest->textfield());
-    if(mRequest->_widgettype_case())      sel->type(mRequest->widgettype());
-    if(mRequest->_widgetstyle_case())     sel->style(mRequest->widgetstyle());
-    if(mRequest->_ischecked_case())       sel->isChecked(mRequest->ischecked());
-    if(mRequest->_ischeckable_case())     sel->isCheckable(mRequest->ischeckable());
-    if(mRequest->_isclickable_case())     sel->isClickable(mRequest->isclickable());
-    if(mRequest->_isenabled_case())       sel->isEnabled(mRequest->isenabled());
-    if(mRequest->_isfocused_case())       sel->isFocused(mRequest->isfocused());
-    if(mRequest->_isfocusable_case())     sel->isFocusable(mRequest->isfocusable());
-    if(mRequest->_isscrollable_case())    sel->isScrollable(mRequest->isscrollable());
-    if(mRequest->_isselected_case())      sel->isSelected(mRequest->isselected());
-    if(mRequest->_isshowing_case())       sel->isShowing(mRequest->isshowing());
-    if(mRequest->_isactive_case())        sel->isActive(mRequest->isactive());
-    if(mRequest->_mindepth_case())        sel->minDepth(mRequest->mindepth());
-    if(mRequest->_maxdepth_case())        sel->maxDepth(mRequest->maxdepth());
-    if(mRequest->_packagename_case())     sel->pkg(mRequest->packagename());
+    if(mRequest->_elementid_case())        sel->id(mRequest->elementid());
+    if(mRequest->_automationid_case())     sel->automationid(mRequest->automationid());
+    if(mRequest->_textfield_case())        sel->text(mRequest->textfield());
+    if(mRequest->_widgettype_case())       sel->type(mRequest->widgettype());
+    if(mRequest->_widgetstyle_case())      sel->style(mRequest->widgetstyle());
+    if(mRequest->_ischecked_case())        sel->isChecked(mRequest->ischecked());
+    if(mRequest->_ischeckable_case())      sel->isCheckable(mRequest->ischeckable());
+    if(mRequest->_isclickable_case())      sel->isClickable(mRequest->isclickable());
+    if(mRequest->_isenabled_case())        sel->isEnabled(mRequest->isenabled());
+    if(mRequest->_isfocused_case())        sel->isFocused(mRequest->isfocused());
+    if(mRequest->_isfocusable_case())      sel->isFocusable(mRequest->isfocusable());
+    if(mRequest->_isscrollable_case())     sel->isScrollable(mRequest->isscrollable());
+    if(mRequest->_isselected_case())       sel->isSelected(mRequest->isselected());
+    if(mRequest->_isshowing_case())        sel->isShowing(mRequest->isshowing());
+    if(mRequest->_isactive_case())         sel->isActive(mRequest->isactive());
+    if(mRequest->_mindepth_case())         sel->minDepth(mRequest->mindepth());
+    if(mRequest->_maxdepth_case())         sel->maxDepth(mRequest->maxdepth());
+    if(mRequest->_packagename_case())      sel->pkg(mRequest->packagename());
+    if(mRequest->_textpartialmatch_case()) sel->textPartialMatch(mRequest->textpartialmatch());
 
     return std::vector<std::shared_ptr<UiSelector>>{sel};
 }
