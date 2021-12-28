6cf3d292b8e60f772b3df2e0ff32d91e5f6a9c7a
Woochanlee <wc0917.lee@samsung.com>
2021-12-28 19:52:49 +0900

    aurum: Introduce ActionAndWaitEvent feature
    Do user request action and wait A11y event. such as window activate, focus changed.
    
    Change-Id: I84534b11bceaad2e1fbcc7e3805bac18e34cfe75

---
+++ org.tizen.aurum-bootstrap/src/Commands/ActionAndWaitEventCommand.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *               http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#include "bootstrap.h"
+#include "ActionAndWaitEventCommand.h"
+#include "ActionAndWaitEventRunnable.h"
+#include "UiDevice.h"
+#include <chrono>
+#include <thread>
+
+ActionAndWaitEventCommand::ActionAndWaitEventCommand(const ::aurum::ReqActionAndWaitEvent *request,
+                                   ::aurum::RspActionAndWaitEvent *response)
+    : mRequest{request}, mResponse{response}
+{
+}
+
+A11yEvent convertEventType(const ::aurum::ReqActionAndWaitEvent_EventType type)
+{
+    if (type == ::aurum::ReqActionAndWaitEvent_EventType::ReqActionAndWaitEvent_EventType_EVENT_WINDOW_ACTIVATE)
+        return A11yEvent::EVENT_WINDOW_ACTIVATE;
+    else if (type == ::aurum::ReqActionAndWaitEvent_EventType::ReqActionAndWaitEvent_EventType_EVENT_WINDOW_DEACTIVATE)
+        return A11yEvent::EVENT_WINDOW_DEACTIVATE;
+    else
+        return A11yEvent::EVENT_STATE_CHANGED_FOCUSED;
+}
+
+::grpc::Status ActionAndWaitEventCommand::execute()
+{
+    LOGI("ActionAndWaitEvent --------------- ");
+    std::unique_ptr<ActionAndWaitEventRunnable> cmd = std::make_unique<ActionAndWaitEventRunnable>(
+                                                      mRequest->type(), mRequest->elementid(), mRequest->xf86keycode());
+    std::shared_ptr<UiDevice> obj = UiDevice::getInstance();
+    obj->executeAndWaitForEvents(cmd.get(), convertEventType(mRequest->eventtype()), mRequest->timeoutms());
+
+    return grpc::Status::OK;
+}
