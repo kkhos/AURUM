7123133f7d3dac102e50986b193acd58eb8681b6
Hosang Kim <hosang12.kim@samsung.com>
2022-03-17 19:59:30 +0900

    aurum: add nodejs test sample for tv
    Change-Id: I648dff347fcd9c12ae81110a1fbc76befb1b2de8

---
+++ protocol/resources/node/tv/searchAll.js
@@ -0,0 +1,97 @@
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import assert, { doesNotMatch } from "assert";
+
+let PROTO_PATH = "./aurum.proto";
+let packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+
+let aurum = grpc.loadPackageDefinition(packageDefinition).aurum;
+
+describe("Aurum Nodejs Test", function () {
+  let client;
+  let searchAll;
+  let textField;
+
+  before(() => {    
+    client = new aurum.Bootstrap(
+      "localhost:50051",
+      grpc.credentials.createInsecure()
+    );
+  });
+  
+  describe("SearchAll Test", () => {
+
+    it("Launch App", function (done) {
+      this.timeout(15000);
+      client.launchApp({ packageName: "com.samsung.tv.searchall" }, (err, response) => {
+        if (err) {
+          console.log(err);
+          done(err);
+          return;
+        }
+        else {
+          assert.strictEqual(response.status, "OK");
+          done();
+          return;
+        }
+      });
+    });
+    
+    /*it("Find SearchAll Window", function (done) {      
+      this.timeout(15000);
+      client.findElement({ maxDepth: 1, packageName: "com.samsung.tv.searchall", isActive: true, isShowing: true }, (err, response) => {
+        if (err) {
+          console.log(err);
+	        done(err);
+        } else {
+          let returnList = response.elements;
+          assert.ok(returnList.length > 0);
+	        
+          console.log(returnList[0]);
+          searchAll = returnList[0];
+          assert.strictEqual(searchAll.package, "com.samsung.tv.searchall");
+          done();
+        }
+      });      
+    });*/
+
+    it("Find TextField", function (done) {      
+      this.timeout(15000);
+      // assert.notEqual(searchAll, undefined);
+
+      client.findElement({ widgetType: "TextField", packageName: "com.samsung.tv.searchall" }, (err, response) => {
+        if (err) {
+          console.log(err);
+	        done(err);
+        } else {
+          let returnList = response.elements;
+          assert.ok(returnList.length > 0);
+	        
+          textField = returnList[0];
+          assert.strictEqual(textField.widget_type, "TextField");
+          done();
+        }
+      });      
+    });
+
+    it("Set Text", function (done) {
+      this.timeout(15000);
+      assert.notEqual(textField, undefined);
+      client.setValue({ elementId: textField.elementId, type: 0, stringValue: "Movie" }, (err, response) => {
+        if (err) {
+          console.log(err);
+	        done(err);
+        } else {
+          assert.strictEqual(response.status, "OK");
+          done();
+        }
+      });      
+    })
+  });
+});
