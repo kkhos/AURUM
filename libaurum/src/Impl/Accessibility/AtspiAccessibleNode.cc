760f0ca0f18961a1444adfa451be88262da48054
Hosang Kim <hosang12.kim@samsung.com>
2022-06-10 14:47:00 +0900

    AccessibleNode: fix return value for setValue
    Change-Id: Ib82f74c691fb830617b85ac469df9df251a01c2e

--- libaurum/src/Impl/Accessibility/AtspiAccessibleNode.cc
+++ libaurum/src/Impl/Accessibility/AtspiAccessibleNode.cc
@@ -440,11 +440,11 @@ bool AtspiAccessibleNode::setValue(std::string text)
     updateName();
     int len = getText().length();
     AtspiWrapper::Atspi_editable_text_delete_text(iface, 0, len, NULL);
-    AtspiWrapper::Atspi_editable_text_insert_text(iface, 0, text.c_str(), text.length(),
-                                                  NULL);
+    bool ret = AtspiWrapper::Atspi_editable_text_insert_text(iface, 0, text.c_str(), text.length(),
+                                                             NULL);
     g_object_unref(iface);
 
-    return true;
+    return ret;
 }
 
 bool AtspiAccessibleNode::setValue(double value)
