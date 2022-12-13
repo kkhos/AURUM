0aa834b1a1e7e181e91f8c28e25565ae96acb795
kim hosang <hosang12.kim@samsung.com>
2022-12-13 11:58:46 +0000

    Revert "aurum: fix using calloc instead of variable-sized array."
    This reverts commit 0f2e8dac34da4f1e5c4d6d0a3537226dd9e99f10.
    
    Change-Id: I39024c57526ce3d060d4744a18a013c9903588d3

--- org.tizen.aurum-bootstrap/src/Commands/TakeScreenshotCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/TakeScreenshotCommand.cc
@@ -50,7 +50,7 @@ TakeScreenshotCommand::TakeScreenshotCommand(
     std::ifstream ifs(path, std::ifstream::binary);
     ::aurum::RspTakeScreenshot rsp;
     int size = mDevice->getScreenSize().width * mDevice->getScreenSize().height;
-    char *buf = (char *) calloc(size, sizeof(char));
+    char buf[size];
 
     while (!ifs.eof()) {
         ifs.read(buf, size);
