b11a6f07a4c7dd8c31aa3aef4ea5374c735f8ffb
Woochan Lee <wc0917.lee@samsung.com>
2023-06-22 15:45:06 +0900

    Add tpk-backend command in setup.py
    The tpk-backend command has been deleted due to an issue at fhub binary creation.
    For proper execution of aurum-bootstrap, tpk-backend should be perfomed at setup time instead.
    
    Change-Id: If4643fe2f93dc39c64555d6952190c3fc1571d53

--- protocol/resources/python/mobile/mobileSetup.py
+++ protocol/resources/python/mobile/mobileSetup.py
@@ -10,6 +10,7 @@ def run_command(command):
 
 # Start scrip here
 run_command("sdb forward tcp:50051 tcp:50051")
+run_command("sdb shell tpk-backend -y org.tizen.aurum-bootstrap --preload")
 run_command("sdb shell app_launcher -s org.tizen.aurum-bootstrap")
 # Wait 1 sec till bootstrap launched
 time.sleep(1)
