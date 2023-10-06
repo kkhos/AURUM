14598e96bc88dadb78988ad456c59e2f31db0fb1
Hosang Kim <hosang12.kim@samsung.com>
2023-10-06 13:41:06 +0900

    sample: remove legacy sample
    Change-Id: I283384897cb1977fd47b3fdaa05137e244b33ed9
    (cherry picked from commit fb511e5cdd0e1e2f5fa87e0a35ef0bc85c12c455)

--- protocol/resources/node/legacySample/gen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./ \
-  --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin`\
-  -I ./../../ ../../aurum.proto
-
