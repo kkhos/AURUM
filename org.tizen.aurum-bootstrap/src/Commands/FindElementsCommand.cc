5b8827d524b365fe1f70040670825325dfaff3c3
Woochan Lee <wc0917.lee@samsung.com>
2023-10-25 20:04:47 +0900

    Delete Screen Analyzer client code
    Screen Analyzer server is not working anymore since 2023/05
    So currently aurum client codes has become useless
    Developed on the client code and the diff is growing so delete all related codes
    
    Change-Id: I8b33c9ded88abfbeff3c6a0420488c2a72e3c07f

--- org.tizen.aurum-bootstrap/src/Commands/FindElementsCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/FindElementsCommand.cc
@@ -22,9 +22,6 @@
 #include "Sel.h"
 #include "ISearchable.h"
 #include "config.h"
-#ifdef MQTT_ENABLED
-#include "SaObject.h"
-#endif
 #include "Until.h"
 
 FindElementsCommand::FindElementsCommand(const ::aurum::ReqFindElements *request,
@@ -87,131 +84,79 @@ std::vector<std::shared_ptr<UiSelector>> FindElementsCommand::getSelectors(void)
     auto selectors     = getSelectors();
     std::shared_ptr<UiDevice> mDevice = UiDevice::getInstance();
 
-#ifdef MQTT_ENABLED
-    if (mDevice->getExternalAppLaunched())
-    {
-        mDevice->RequestScreenAnalyze();
+    std::vector<std::shared_ptr<UiObject>> founds = {};
 
-        std::vector<std::shared_ptr<SaObject>> founds = {};
+    auto waiter = new Waiter(searchableObj.get(), nullptr, mTimeout);
 
-        for ( auto &sel : selectors ) {
-            auto ret = mDevice->getSAWatcher()->findSaObjects(sel);
-            std::move(std::begin(ret), std::end(ret), std::back_inserter(founds));
-        }
-        if (founds.size() > 0) {
-            for (auto& found : founds) {
-                SaObject *obj = found.get();
-                ::aurum::Element *elm = mResponse->add_elements();
-                elm->set_elementid(obj->getId());
-
-                ::aurum::Rect *rect = elm->mutable_geometry();
-                const Rect<int> &size = obj->getScreenBoundingBox();
-                rect->set_x(size.mTopLeft.x);
-                rect->set_y(size.mTopLeft.y);
-                rect->set_width(size.width());
-                rect->set_height(size.height());
-
-                elm->set_widgettype(obj->getType());
-
-                elm->set_text(obj->getOcrText());
-                elm->set_toolkit("External");
-
-                elm->set_isclickable(obj->isClickable());
-                elm->set_isfocused(obj->isFocused());
-                elm->set_isfocusable(obj->isFocusable());
-                elm->set_isactive(obj->isActive());
-                elm->set_isshowing(true);
-                elm->set_isvisible(true);
-            }
-                mResponse->set_status(::aurum::RspStatus::OK);
-        }
-        else {
-            mResponse->set_status(::aurum::RspStatus::ERROR);
-        }
+    for (auto &sel : selectors) {
+        auto ret = waiter->waitFor(Until::findObjects(sel));
+        std::move(std::begin(ret), std::end(ret), std::back_inserter(founds));
     }
-    else
-#endif
-    {
-        if (mDevice->getWithScreenAnalyzer())
-        {
-            LOGI("Find object with SA information");
-            mDevice->RequestScreenAnalyze();
+
+    if (waiter) delete waiter;
+
+    if (founds.size() > 0) {
+        for (auto &found : founds) {
+            UiObject *obj = found.get();
+            obj->refresh();
+            if (mObjMap->getElement(obj->getId()) == nullptr)
+                mObjMap->addElement(std::move(found));
+
+            LOGI("found object : %p elementId:%s", obj, obj->getId().c_str());
+
+            ::aurum::Element *elm = mResponse->add_elements();
+            elm->set_elementid(obj->getId());
+            elm->set_package(obj->getApplicationPackage());
+
+            ::aurum::Rect   *rect = elm->mutable_geometry();
+            const Rect<int> &size = obj->getScreenBoundingBox();
+            rect->set_x(size.mTopLeft.x);
+            rect->set_y(size.mTopLeft.y);
+            rect->set_width(size.width());
+            rect->set_height(size.height());
+
+            ::aurum::Rect   *windowRect = elm->mutable_windowrelativegeometry();
+            const Rect<int> &windowRelativeSize = obj->getWindowBoundingBox();
+            windowRect->set_x(windowRelativeSize.mTopLeft.x);
+            windowRect->set_y(windowRelativeSize.mTopLeft.y);
+            windowRect->set_width(windowRelativeSize.width());
+            windowRect->set_height(windowRelativeSize.height());
+
+            elm->set_widgettype(obj->getType());
+            elm->set_widgetstyle(obj->getElementStyle());
+
+            elm->set_text(obj->getText());
+            elm->set_xpath(obj->getXPath());
+            elm->set_ocrtext(obj->getOcrText());
+            elm->set_automationid(obj->getAutomationId());
+            elm->set_package(obj->getApplicationPackage());
+            elm->set_role(obj->getRole());
+
+            elm->set_ischecked(obj->isChecked());
+            elm->set_ischeckable(obj->isCheckable());
+            elm->set_isclickable(obj->isClickable());
+            elm->set_isenabled(obj->isEnabled());
+            elm->set_isfocused(obj->isFocused());
+            elm->set_isfocusable(obj->isFocusable());
+            elm->set_isscrollable(obj->isScrollable());
+            elm->set_isselected(obj->isSelected());
+            elm->set_isshowing(obj->isShowing());
+            elm->set_isactive(obj->isActive());
+            elm->set_isvisible(obj->isVisible());
+            elm->set_isselectable(obj->isSelectable());
+            elm->set_ishighlightable(obj->isHighlightable());
+
+            elm->set_minvalue(obj->getMinValue());
+            elm->set_maxvalue(obj->getMaxValue());
+            elm->set_value(obj->getValue());
+            elm->set_increment(obj->getIncrement());
+
+            elm->set_windowangle(obj->getWindowAngle());
+            elm->set_targetangle(obj->getTargetAngle());
         }
-
-        std::vector<std::shared_ptr<UiObject>> founds = {};
-
-        auto waiter = new Waiter(searchableObj.get(), nullptr, mTimeout);
-
-        for ( auto &sel : selectors ) {
-            auto ret = waiter->waitFor(Until::findObjects(sel));
-            std::move(std::begin(ret), std::end(ret), std::back_inserter(founds));
-        }
-
-        if (waiter) delete waiter;
-
-        if (founds.size() > 0) {
-            for (auto& found : founds) {
-                UiObject *obj = found.get();
-                obj->refresh();
-                if (mObjMap->getElement(obj->getId()) == nullptr)
-                    mObjMap->addElement(std::move(found));
-
-                LOGI("found object : %p elementId:%s", obj, obj->getId().c_str());
-
-                ::aurum::Element *elm = mResponse->add_elements();
-                elm->set_elementid(obj->getId());
-                elm->set_package(obj->getApplicationPackage());
-
-                ::aurum::Rect *rect = elm->mutable_geometry();
-                const Rect<int> &size = obj->getScreenBoundingBox();
-                rect->set_x(size.mTopLeft.x);
-                rect->set_y(size.mTopLeft.y);
-                rect->set_width(size.width());
-                rect->set_height(size.height());
-
-                ::aurum::Rect *windowRect = elm->mutable_windowrelativegeometry();
-                const Rect<int> &windowRelativeSize = obj->getWindowBoundingBox();
-                windowRect->set_x(windowRelativeSize.mTopLeft.x);
-                windowRect->set_y(windowRelativeSize.mTopLeft.y);
-                windowRect->set_width(windowRelativeSize.width());
-                windowRect->set_height(windowRelativeSize.height());
-
-                elm->set_widgettype(obj->getType());
-                elm->set_widgetstyle(obj->getElementStyle());
-
-                elm->set_text(obj->getText());
-                elm->set_xpath(obj->getXPath());
-                elm->set_ocrtext(obj->getOcrText());
-                elm->set_automationid(obj->getAutomationId());
-                elm->set_package(obj->getApplicationPackage());
-                elm->set_role(obj->getRole());
-
-                elm->set_ischecked(obj->isChecked());
-                elm->set_ischeckable(obj->isCheckable());
-                elm->set_isclickable(obj->isClickable());
-                elm->set_isenabled(obj->isEnabled());
-                elm->set_isfocused(obj->isFocused());
-                elm->set_isfocusable(obj->isFocusable());
-                elm->set_isscrollable(obj->isScrollable());
-                elm->set_isselected(obj->isSelected());
-                elm->set_isshowing(obj->isShowing());
-                elm->set_isactive(obj->isActive());
-                elm->set_isvisible(obj->isVisible());
-                elm->set_isselectable(obj->isSelectable());
-                elm->set_ishighlightable(obj->isHighlightable());
-
-                elm->set_minvalue(obj->getMinValue());
-                elm->set_maxvalue(obj->getMaxValue());
-                elm->set_value(obj->getValue());
-                elm->set_increment(obj->getIncrement());
-
-                elm->set_windowangle(obj->getWindowAngle());
-                elm->set_targetangle(obj->getTargetAngle());
-            }
-            mResponse->set_status(::aurum::RspStatus::OK);
-        } else {
-            mResponse->set_status(::aurum::RspStatus::ERROR);
-        }
+        mResponse->set_status(::aurum::RspStatus::OK);
+    } else {
+        mResponse->set_status(::aurum::RspStatus::ERROR);
     }
 
     return grpc::Status::OK;
--- org.tizen.aurum-bootstrap/src/Commands/GetSizeCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/GetSizeCommand.cc
@@ -22,10 +22,6 @@
 #include "Sel.h"
 #include "ISearchable.h"
 #include "config.h"
-#ifdef MQTT_ENABLED
-#include "SaObject.h"
-#endif
-
 
 GetSizeCommand::GetSizeCommand(const ::aurum::ReqGetSize *request,
                                ::aurum::RspGetSize *response)
@@ -39,56 +35,25 @@ GetSizeCommand::GetSizeCommand(const ::aurum::ReqGetSize *request,
 
     std::shared_ptr<UiDevice> mDevice = UiDevice::getInstance();
 
-#ifdef MQTT_ENABLED
-    if (mDevice->getExternalAppLaunched())
-    {
-        mDevice->RequestScreenAnalyze();
-
-        std::vector<std::shared_ptr<SaObject>> founds = {};
-
-        auto tempSel = std::make_shared<UiSelector>();
-        tempSel->id(mRequest->elementid());
-        auto selectors  = std::vector<std::shared_ptr<UiSelector>>{tempSel};
-
-        for ( auto &sel : selectors ) {
-            auto ret = mDevice->getSAWatcher()->findSaObjects(sel);
-            std::move(std::begin(ret), std::end(ret), std::back_inserter(founds));
-        }
-        if (founds.size() > 0) {
-            for (auto& found : founds) {
-                SaObject *obj = found.get();
-                ::aurum::Rect *rect = mResponse->mutable_size();
-                const Rect<int> &size = obj->getScreenBoundingBox();
-                rect->set_x(size.mTopLeft.x);
-                rect->set_y(size.mTopLeft.y);
-                rect->set_width(size.width());
-                rect->set_height(size.height());
-            }
-        }
-    }
-    else
-#endif
-    {
-        ::aurum::ReqGetSize_CoordType type = mRequest->type();
-        ObjectMapper *mObjMap = ObjectMapper::getInstance();
-        std::shared_ptr<UiObject> obj = mObjMap->getElement(mRequest->elementid());
-        if (obj) {
-            obj->updateExtents();
-            ::aurum::Rect *rect = mResponse->mutable_size();
-            if (type == ::aurum::ReqGetSize_CoordType::ReqGetSize_CoordType_SCREEN) {
-                const Rect<int> &size = obj->getScreenBoundingBox();
-                rect->set_x(size.mTopLeft.x);
-                rect->set_y(size.mTopLeft.y);
-                rect->set_width(size.width());
-                rect->set_height(size.height());
-            }
-            else {
-                const Rect<int> &windowRelativeSize = obj->getWindowBoundingBox();
-                rect->set_x(windowRelativeSize.mTopLeft.x);
-                rect->set_y(windowRelativeSize.mTopLeft.y);
-                rect->set_width(windowRelativeSize.width());
-                rect->set_height(windowRelativeSize.height());
-            }
+    ::aurum::ReqGetSize_CoordType type = mRequest->type();
+    ObjectMapper                 *mObjMap = ObjectMapper::getInstance();
+    std::shared_ptr<UiObject> obj = mObjMap->getElement(mRequest->elementid());
+    if (obj) {
+        obj->updateExtents();
+        ::aurum::Rect *rect = mResponse->mutable_size();
+        if (type ==
+            ::aurum::ReqGetSize_CoordType::ReqGetSize_CoordType_SCREEN) {
+            const Rect<int> &size = obj->getScreenBoundingBox();
+            rect->set_x(size.mTopLeft.x);
+            rect->set_y(size.mTopLeft.y);
+            rect->set_width(size.width());
+            rect->set_height(size.height());
+        } else {
+            const Rect<int> &windowRelativeSize = obj->getWindowBoundingBox();
+            rect->set_x(windowRelativeSize.mTopLeft.x);
+            rect->set_y(windowRelativeSize.mTopLeft.y);
+            rect->set_width(windowRelativeSize.width());
+            rect->set_height(windowRelativeSize.height());
         }
     }
 
