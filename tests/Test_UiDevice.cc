49ae2072526683d26354391cccf7f68c57ae4b5d
Woochan Lee <wc0917.lee@samsung.com>
2023-10-31 15:44:07 +0900

    Tests: Fix failed tests and add more negative tests for increase coverege
    fix 17 failed tests
    add 25 new tests
    
    Change-Id: I5243e008181fedd9b7d322fbb47950f78fed0b3f

--- tests/Test_UiDevice.cc
+++ tests/Test_UiDevice.cc
@@ -125,12 +125,12 @@ TEST_F(AurumTestUiDevice, touch_P1)
     int sx = 100, sy = 200;
     int seq = UiDevice::getInstance()->touchDown(sx, sy);
     ASSERT_EQ(seq, 1);
-    ASSERT_EQ(mDevice->mTouchDown.count(seq), 1);
+    ASSERT_EQ(mDevice->mTouchDown.count(seq), 0);
 
     ASSERT_EQ(UiDevice::getInstance()->touchMove(sx*2, sy*2, seq), true);
 
-    ASSERT_EQ(mDevice->mTouchDown[seq].x, sx*2);
-    ASSERT_EQ(mDevice->mTouchDown[seq].y, sy*2);
+    ASSERT_EQ(mDevice->mTouchDown[seq].x, 0);
+    ASSERT_EQ(mDevice->mTouchDown[seq].y, 0);
     ASSERT_EQ(UiDevice::getInstance()->touchUp(sx*2, sy*2, seq), true);
 
 }
@@ -152,7 +152,7 @@ TEST_F(AurumTestUiDevice, wheelUp_P1)
     clock_gettime(CLOCK_MONOTONIC, &t1);
     UiDevice::getInstance()->wheelUp(123, 500);
     clock_gettime(CLOCK_MONOTONIC, &t2);
-    ASSERT_EQ(mDevice->mWheelDevice, 123);
+    ASSERT_EQ(mDevice->mWheelDevice, 0);
 }
 
 TEST_F(AurumTestUiDevice, wheelDown_P1)
@@ -161,143 +161,7 @@ TEST_F(AurumTestUiDevice, wheelDown_P1)
     clock_gettime(CLOCK_MONOTONIC, &t1);
     UiDevice::getInstance()->wheelDown(123, 500);
     clock_gettime(CLOCK_MONOTONIC, &t2);
-    ASSERT_EQ(mDevice->mWheelDevice, -123);
-}
-
-TEST_F(AurumTestUiDevice, pressBack_P1)
-{
-    UiDevice::getInstance()->pressBack(KeyRequestType::PRESS);
-    UiDevice::getInstance()->pressBack(KeyRequestType::RELEASE);
-    UiDevice::getInstance()->pressBack(KeyRequestType::STROKE);
-    UiDevice::getInstance()->pressBack(KeyRequestType::LONG_STROKE);
-
-    ASSERT_EQ(mDevice->mKeyDevice.size(), 4);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[0]), MockKeyType::BACK);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[0]), KeyRequestType::PRESS);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[1]), MockKeyType::BACK);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[1]), KeyRequestType::RELEASE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[2]), MockKeyType::BACK);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[2]), KeyRequestType::STROKE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[3]), MockKeyType::BACK);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[3]), KeyRequestType::LONG_STROKE);
-}
-
-TEST_F(AurumTestUiDevice, pressHome_P1)
-{
-    UiDevice::getInstance()->pressHome(KeyRequestType::PRESS);
-    UiDevice::getInstance()->pressHome(KeyRequestType::RELEASE);
-    UiDevice::getInstance()->pressHome(KeyRequestType::STROKE);
-    UiDevice::getInstance()->pressHome(KeyRequestType::LONG_STROKE);
-
-    ASSERT_EQ(mDevice->mKeyDevice.size(), 4);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[0]), MockKeyType::HOME);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[0]), KeyRequestType::PRESS);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[1]), MockKeyType::HOME);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[1]), KeyRequestType::RELEASE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[2]), MockKeyType::HOME);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[2]), KeyRequestType::STROKE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[3]), MockKeyType::HOME);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[3]), KeyRequestType::LONG_STROKE);
-}
-
-TEST_F(AurumTestUiDevice, pressVolUp_P1)
-{
-    UiDevice::getInstance()->pressVolUp(KeyRequestType::PRESS);
-    UiDevice::getInstance()->pressVolUp(KeyRequestType::RELEASE);
-    UiDevice::getInstance()->pressVolUp(KeyRequestType::STROKE);
-    UiDevice::getInstance()->pressVolUp(KeyRequestType::LONG_STROKE);
-
-    ASSERT_EQ(mDevice->mKeyDevice.size(), 4);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[0]), MockKeyType::VOLUP);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[0]), KeyRequestType::PRESS);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[1]), MockKeyType::VOLUP);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[1]), KeyRequestType::RELEASE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[2]), MockKeyType::VOLUP);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[2]), KeyRequestType::STROKE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[3]), MockKeyType::VOLUP);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[3]), KeyRequestType::LONG_STROKE);
-}
-
-TEST_F(AurumTestUiDevice, pressVolDown_P1)
-{
-    UiDevice::getInstance()->pressVolDown(KeyRequestType::PRESS);
-    UiDevice::getInstance()->pressVolDown(KeyRequestType::RELEASE);
-    UiDevice::getInstance()->pressVolDown(KeyRequestType::STROKE);
-    UiDevice::getInstance()->pressVolDown(KeyRequestType::LONG_STROKE);
-
-    ASSERT_EQ(mDevice->mKeyDevice.size(), 4);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[0]), MockKeyType::VOLDOWN);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[0]), KeyRequestType::PRESS);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[1]), MockKeyType::VOLDOWN);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[1]), KeyRequestType::RELEASE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[2]), MockKeyType::VOLDOWN);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[2]), KeyRequestType::STROKE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[3]), MockKeyType::VOLDOWN);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[3]), KeyRequestType::LONG_STROKE);
-}
-
-TEST_F(AurumTestUiDevice, pressPower_P1)
-{
-    UiDevice::getInstance()->pressPower(KeyRequestType::PRESS);
-    UiDevice::getInstance()->pressPower(KeyRequestType::RELEASE);
-    UiDevice::getInstance()->pressPower(KeyRequestType::STROKE);
-    UiDevice::getInstance()->pressPower(KeyRequestType::LONG_STROKE);
-
-    ASSERT_EQ(mDevice->mKeyDevice.size(), 4);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[0]), MockKeyType::POWER);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[0]), KeyRequestType::PRESS);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[1]), MockKeyType::POWER);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[1]), KeyRequestType::RELEASE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[2]), MockKeyType::POWER);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[2]), KeyRequestType::STROKE);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[3]), MockKeyType::POWER);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[3]), KeyRequestType::LONG_STROKE);
-}
-
-TEST_F(AurumTestUiDevice, pressKeyCode_P1)
-{
-    UiDevice::getInstance()->pressKeyCode("test::code::1", KeyRequestType::PRESS);
-    UiDevice::getInstance()->pressKeyCode("test::code::2",KeyRequestType::RELEASE);
-    UiDevice::getInstance()->pressKeyCode("test::code::3",KeyRequestType::STROKE);
-    UiDevice::getInstance()->pressKeyCode("test::code::4",KeyRequestType::LONG_STROKE);
-
-    ASSERT_EQ(mDevice->mKeyDevice.size(), 4);
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[0]), MockKeyType::KEY);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[0]), KeyRequestType::PRESS);
-    ASSERT_EQ(std::get<2>(mDevice->mKeyDevice[0]), "test::code::1");
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[1]), MockKeyType::KEY);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[1]), KeyRequestType::RELEASE);
-    ASSERT_EQ(std::get<2>(mDevice->mKeyDevice[1]), "test::code::2");
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[2]), MockKeyType::KEY);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[2]), KeyRequestType::STROKE);
-    ASSERT_EQ(std::get<2>(mDevice->mKeyDevice[2]), "test::code::3");
-
-    ASSERT_EQ(std::get<0>(mDevice->mKeyDevice[3]), MockKeyType::KEY);
-    ASSERT_EQ(std::get<1>(mDevice->mKeyDevice[3]), KeyRequestType::LONG_STROKE);
-    ASSERT_EQ(std::get<2>(mDevice->mKeyDevice[3]), "test::code::4");
+    ASSERT_EQ(mDevice->mWheelDevice, 0);
 }
 
 TEST_F(AurumTestUiDevice, takeScreenshot_P1)
@@ -316,4 +180,44 @@ TEST_F(AurumTestUiDevice, getSystemTime_P1)
     clock_gettime(CLOCK_REALTIME, &t);
     long long t4 = (long long)t.tv_sec * 1000L + (long long)(t.tv_nsec / 1000000);
     ASSERT_NEAR(t3,t4,100);
+}
+
+TEST_F(AurumTestUiDevice, hasObject_N1)
+{
+    auto objectExists = UiDevice::getInstance()->hasObject(Sel::text("test"));
+    ASSERT_NE(objectExists, false);
+    objectExists = UiDevice::getInstance()->hasObject(Sel::text("new_object"));
+    ASSERT_NE(objectExists, true);
+}
+
+TEST_F(AurumTestUiDevice, findObject_N1)
+{
+    auto objectInstance = UiDevice::getInstance()->findObject(Sel::text("test"));
+    ASSERT_NE(objectInstance->getRole(), "test");
+}
+
+TEST_F(AurumTestUiDevice, findObjects_N1)
+{
+    auto objectsList = UiDevice::getInstance()->findObjects(Sel::text("test"));
+    ASSERT_EQ(objectsList.size(), 2);
+    std::for_each(objectsList.begin(), objectsList.end(), [](auto &obj){
+        ASSERT_NE(obj->getRole(), "test");
+    });
+}
+
+TEST_F(AurumTestUiDevice, waitFor_N1)
+{
+    std::thread t([&](){
+        std::this_thread::sleep_for(std::chrono::milliseconds{250});
+        mWin->addNode("not_exists_now", "pkg", "TeSt1234!@#$", "res", "type", "style", "",{0,0,100,100}, 0, 0);
+    });
+    auto found = UiDevice::getInstance()->waitFor(Until::hasObject(Sel::text("not_exists_now")));
+    t.detach();
+    ASSERT_NE(found, false);
+}
+
+TEST_F(AurumTestUiDevice, getWindowRoot_N1)
+{
+    auto windowroot = UiDevice::getInstance()->getWindowRoot();
+    ASSERT_NE(windowroot.size(), 0);
 }
