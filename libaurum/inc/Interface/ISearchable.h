43f181a56adeeac4e20689d6eadf8c47919f5319
Hosang Kim <hosang12.kim@samsung.com>
2024-01-26 18:20:57 +0900

    libaurum: Add IMatches interface for common methods
    Implement the IMatches abstract class in UiObject and UiDevice
    to improve code readability and maintainability by separating interface from implementation.
    
    Change-Id: I2ef4d467024f0971622b7f97bba1a6daee30b15a

--- libaurum/inc/Interface/ISearchable.h
+++ libaurum/inc/Interface/ISearchable.h
@@ -79,6 +79,38 @@ public:
      */
     virtual std::vector<std::shared_ptr<UiObject>> findObjects(
         const std::shared_ptr<UiSelector> selector) const = 0;
+
+    /**
+     * @brief Get the Matches object that satisfied with the selector condition in the object tree.
+     *        This method is similar to findObjects, but getMatches is more faster than findObjects.
+     *        A single dbus method call occurs internally and retrieves a list of objects
+     *        that satisfy the selector specified in uifw.
+     *
+     * @param[in] selector @UiSelector
+     * @param[in] earlyReturn boolean Returns immediately when a matched object is found
+     *
+     * @return the list of found UiObject pointer vector
+     *
+     * @since_tizen 8.0
+     */
+    virtual std::vector<std::shared_ptr<UiObject>> getMatches(
+        const std::shared_ptr<UiSelector> selector, const bool earlyReturn) const = 0;
+
+    /**
+     * @brief Get the object that satisfied with both condition in the object tree.
+     *        Additional object detection is performed using the second selector for the object
+     *        found in the first selector.
+     *
+     * @param[in] firstSelector @UiSelector
+     * @param[in] secondSelector @UiSelector
+     * @param[in] earlyReturn boolean Returns immediately when a matched object is found
+     *
+     * @return the list of found UiObject pointer vector
+     *
+     * @since_tizen 8.0
+     */
+    virtual std::vector<std::shared_ptr<UiObject>> getMatchesInMatches(
+        const std::shared_ptr<UiSelector> firstSelector, const std::shared_ptr<UiSelector> secondSelector, const bool earlyReturn) const = 0;
 };
 
 }
