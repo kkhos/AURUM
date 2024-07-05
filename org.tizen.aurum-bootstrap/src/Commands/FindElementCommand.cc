a7801572c8dd8d3c825c5095c6cfec2999c9450c
Hosang Kim <hosang12.kim@samsung.com>
2024-07-05 13:16:02 +0900

    Aurum: fix svace issue
    Change-Id: I0394aa3bc7754277c224c26bf7a3b00a9689de5c

--- org.tizen.aurum-bootstrap/src/Commands/FindElementCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/FindElementCommand.cc
@@ -88,7 +88,7 @@ std::shared_ptr<UiSelector> FindElementCommand::getSelector(void)
     auto waiter = new Waiter(searchableObj.get(), nullptr, mTimeout);
     auto found = waiter->waitFor(Until::findObject(selector));
 
-    if (waiter) delete waiter;
+    delete waiter;
 
     if (found != nullptr) {
         UiObject *obj = found.get();
