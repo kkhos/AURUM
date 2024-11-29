84cd2b441dd39cbfdb93046efaff66a4cce398cf
Hosang Kim <hosang12.kim@samsung.com>
2024-11-29 13:44:10 +0900

    aurum: add image source information
    Change-Id: I92c9ec52a12a1a31dd55e6b810f58f1d0db76ae0

--- org.tizen.aurum-bootstrap/src/Commands/LastCommand.cc
+++ org.tizen.aurum-bootstrap/src/Commands/LastCommand.cc
@@ -98,6 +98,7 @@ LastCommand::LastCommand(const ::aurum::ReqLast *request,
 
         elm->set_interface(obj->getInterface());
         elm->set_description(obj->getDescription());
+        elm->set_imgsrc(obj->getImgSrc());
 
         mResponse->set_status(::aurum::RspStatus::OK);
     } else {
